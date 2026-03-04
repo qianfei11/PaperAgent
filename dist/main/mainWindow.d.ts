8121c5ea59570720f4e9305fb9e1aa84a99615f3
qianfei11 <490584635@qq.com>
2026-03-04 21:50:10 +0800

    remove dist/

--- dist/main/mainWindow.d.ts
+++
@@ -1,3 +0,0 @@
-import { BrowserWindow } from 'electron';
-export declare function createMainWindow(): BrowserWindow;
-//# sourceMappingURL=mainWindow.d.ts.map
--- dist/renderer/scripts/main.d.ts
+++
@@ -1,7 +0,0 @@
-declare global {
-    interface Window {
-        electronAPI: any;
-    }
-}
-export {};
-//# sourceMappingURL=main.d.ts.map
